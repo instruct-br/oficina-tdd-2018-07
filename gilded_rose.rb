c89757f40977ee6e1019b2ba65af0934b419978f
Oscar Esgalha <oscar@instruct.com.br>
2018-07-12 09:55:18 -0300

    Sobe o código ruim

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -6,6 +6,43 @@ class GildedRose
   end
 
   def tick
-    # TODO :)
+    if @name != 'Aged Brie' && @name != 'Backstage passes to a TAKFAL80ETC concert'
+      if @quality > 0
+        if @name != 'Sulfuras, Hand of Ragnaros'
+          @quality -= 1
+        end
+      end
+    else
+      if @quality < 50
+        @quality += 1
+        if @name == 'Backstage passes to a TAKFAL80ETC concert'
+          if @days_remaining < 11
+            if @quality < 50
+              @quality += 1
+            end
+          end
+        end
+      end
+    end
+    if @name != 'Sulfuras, Hand of Ragnaros'
+      @days_remaining -= 1
+    end
+    if @days_remaining < 0
+      if @name != 'Aged Brie'
+        if @name != 'Backstage passes to a TAKFAL80ETC concert'
+          if @quality > 0
+            if @name != 'Sulfuras, Hand of Ragnaros'
+              @quality -= 1
+            end
+          end
+        else
+          @quality = @quality - @quality
+        end
+      else
+        if @quality < 50
+          @quality += 1
+        end
+      end
+    end
   end
 end
